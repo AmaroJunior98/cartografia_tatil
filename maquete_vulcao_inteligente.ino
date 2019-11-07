c68417450c24087eff951921d82cbf95628d6456
AmaroJunior98 <porfirioamarojr@gmail.com>
2019-11-07 17:58:38 -0300

    Init

---
+++ maquete_vulcao_inteligente.ino
@@ -0,0 +1,22 @@
+#define button1 12
+#define button2 11
+#define button3 10
+#define button4 9
+#define button5 8
+#define button6 7
+
+void setup(){
+    pinMode(button1, INPUT_PULLUP);
+    pinMode(button2, INPUT_PULLUP);
+    pinMode(button3, INPUT_PULLUP);
+    pinMode(button4, INPUT_PULLUP);
+    pinMode(button5, INPUT_PULLUP);
+    pinMode(button6, INPUT_PULLUP);
+
+}
+
+void loop(){
+
+
+}
+
